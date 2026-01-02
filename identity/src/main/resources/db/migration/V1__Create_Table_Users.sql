a062e265ebd1b5bb0f87dd1f83a06e9c1191c8f6
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-02 17:24:57 -0300

    feat(identity/resources/db/migrations): Create users table with necessary fields

---
+++ identity/src/main/resources/db/migration/V1__Create_Table_Users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users
+(
+    id            UUID                        NOT NULL,
+    avatar        VARCHAR(255),
+    first_name    VARCHAR(255)                NOT NULL,
+    last_name     VARCHAR(255)                NOT NULL,
+    role          VARCHAR(255)                NOT NULL,
+    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    is_active     BOOLEAN                     NOT NULL,
+    email_address VARCHAR(255),
+    password      VARCHAR(255),
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
