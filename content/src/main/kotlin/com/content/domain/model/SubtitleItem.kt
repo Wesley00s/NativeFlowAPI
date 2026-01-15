2d2bb20e7d65904cbac6ae63b1b580bbb470c19e
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:32:53 -0300

    feat(model): Add unique ID generation to TranscriptionSegment

--- content/src/main/kotlin/com/content/domain/model/SubtitleItem.kt
+++
@@ -1,8 +0,0 @@
-package com.content.domain.model
-
-data class SubtitleItem(
-    val text: String,
-    val start: Double,
-    val end: Double,
-    val conf: Double? = null
-)
--- content/src/main/kotlin/com/content/domain/model/TranscriptionSegment.kt
+++ content/src/main/kotlin/com/content/domain/model/TranscriptionSegment.kt
@@ -1,6 +1,9 @@
 package com.content.domain.model
 
+import com.github.f4b6a3.uuid.UuidCreator
+
 data class TranscriptionSegment(
+    val id: String = UuidCreator.getTimeOrderedEpoch().toString(),
     val text: String,
     val start: Double,
     val end: Double,
