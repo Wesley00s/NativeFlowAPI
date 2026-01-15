afec0d84de5d11c5988ecb24b96d36a67f78fb63
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:28:13 -0300

    refactor(database): Rename users table to user_tb and update email column

--- identity/src/main/resources/db/migration/V1__Create_Table_Users.sql
+++ identity/src/main/resources/db/migration/V1__Create_Table_Users.sql
@@ -1,13 +1,13 @@
-CREATE TABLE users
+CREATE TABLE user_tb
 (
-    id            UUID                        NOT NULL,
-    avatar        VARCHAR(255),
-    first_name    VARCHAR(255)                NOT NULL,
-    last_name     VARCHAR(255)                NOT NULL,
-    role          VARCHAR(255)                NOT NULL,
-    created_at    TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    is_active     BOOLEAN                     NOT NULL,
-    email_address VARCHAR(255),
-    password      VARCHAR(255),
+    id         UUID                        NOT NULL,
+    avatar     VARCHAR(255),
+    first_name VARCHAR(255)                NOT NULL,
+    last_name  VARCHAR(255)                NOT NULL,
+    role       VARCHAR(255)                NOT NULL,
+    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    is_active  BOOLEAN                     NOT NULL,
+    email      VARCHAR(255),
+    password   VARCHAR(255),
     CONSTRAINT pk_users PRIMARY KEY (id)
 );
