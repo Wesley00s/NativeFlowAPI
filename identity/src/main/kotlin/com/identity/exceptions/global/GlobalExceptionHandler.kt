95f3deeace4f18892ab65d8fa73195ebb269a46c
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:28:54 -0300

    feat(exceptions): Enhance GlobalExceptionHandler with additional exception handling

--- identity/src/main/kotlin/com/identity/exceptions/global/GlobalExceptionHandler.kt
+++ identity/src/main/kotlin/com/identity/exceptions/global/GlobalExceptionHandler.kt
@@ -1,16 +1,70 @@
 package com.identity.exceptions.global
 
 import com.identity.exceptions.local.InvalidTokenException
+import com.identity.exceptions.model.ExceptionDetails
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.FieldError
+import org.springframework.validation.ObjectError
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.multipart.MultipartException
+import java.nio.file.AccessDeniedException
+import java.time.Instant
+import java.util.function.Consumer
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidException(ex: MethodArgumentNotValidException): ResponseEntity<ExceptionDetails> {
+        val errors: MutableMap<String?, String?> = HashMap()
+        ex.bindingResult.allErrors.forEach(Consumer { error: ObjectError? ->
+            val fieldName = if (error is FieldError) error.field else error!!.objectName
+            errors[fieldName] = error.defaultMessage
+        })
+        return ResponseEntity.badRequest().body(
+            ExceptionDetails(
+                "Bad Request: Invalid fields",
+                Instant.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                ex.javaClass.getName(),
+                errors
+            )
+        )
+    }
+
     @ExceptionHandler(InvalidTokenException::class)
-    fun handleInvalidTokenException(ex: InvalidTokenException): ResponseError {
-        return ResponseError(
-            message = ex.message,
-            details = ex.cause?.message
+    fun handleInvalidTokenException(ex: InvalidTokenException): ResponseEntity<ExceptionDetails> {
+        return ExceptionsUtils.createErrorResponse(ex, ex.message, HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(EntityNotFoundException::class, NoSuchElementException::class)
+    fun handleNotFound(ex: Exception): ResponseEntity<ExceptionDetails> {
+        return ExceptionsUtils.createErrorResponse(ex, "Resource not found.", HttpStatus.NOT_FOUND)
+    }
+
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDenied(ex: AccessDeniedException): ResponseEntity<ExceptionDetails> {
+        return ExceptionsUtils.createErrorResponse(ex, "Access Denied.", HttpStatus.FORBIDDEN)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGenericException(ex: Exception): ResponseEntity<ExceptionDetails> {
+        return ExceptionsUtils.createErrorResponse(
+            ex,
+            "An unexpected internal error occurred.",
+            HttpStatus.INTERNAL_SERVER_ERROR
+        )
+    }
+
+    @ExceptionHandler(MultipartException::class)
+    fun handleMultipartException(ex: MultipartException): ResponseEntity<ExceptionDetails> {
+        return ExceptionsUtils.createErrorResponse(
+            ex,
+            "Upload Error: The request is malformed or incomplete.",
+            HttpStatus.BAD_REQUEST
         )
     }
 }
