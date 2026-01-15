ba4dc129b0ed60f26ca1775886c160d0140003bc
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:30:09 -0300

    feat(token): Add token generation with expiration support in TokenService

--- identity/src/main/kotlin/com/identity/config/security/TokenService.kt
+++ identity/src/main/kotlin/com/identity/config/security/TokenService.kt
@@ -6,13 +6,29 @@ import com.identity.exceptions.local.InvalidTokenException
 import org.springframework.stereotype.Service
 import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
+import java.time.Instant
+import java.util.Date
+import java.util.UUID
 
 @Service
 class TokenService(
     val publicKey: RSAPublicKey,
     val privateKey: RSAPrivateKey,
-    val issuer: String
+    val issuer: String,
+    val expiration: Long
 ) {
+    fun generateToken(userId: UUID): String {
+        val algorithm = Algorithm.RSA256(publicKey, privateKey)
+        return JWT.create()
+            .withIssuer(issuer)
+            .withSubject(userId.toString())
+            .withIssuedAt(Instant.now())
+            .withExpiresAt(Date.from(Instant.now().plusMillis(expiration)))
+            .sign(algorithm);
+
+    }
+
+
     fun validateToken(token: String): String? = try {
         val algorithm = Algorithm.RSA256(publicKey, privateKey)
         JWT.require(algorithm)
