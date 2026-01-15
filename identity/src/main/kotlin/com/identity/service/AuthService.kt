e53395ed69af6fb681dd4b0010e6a1b46a576b91
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:28:28 -0300

    feat(auth): Implement authentication service with sign up and sign in functionality

---
+++ identity/src/main/kotlin/com/identity/service/AuthService.kt
@@ -0,0 +1,64 @@
+package com.identity.service
+
+import com.identity.api.dto.request.SignInRequest
+import com.identity.api.dto.request.SignUpRequest
+import com.identity.api.dto.response.SignInResponse
+import com.identity.api.dto.response.SignUpResponse
+import com.identity.config.security.TokenService
+import com.identity.domain.mapper.SignInMapper
+import com.identity.domain.mapper.SignUpMapper
+import com.identity.domain.model.Password
+import com.identity.domain.repository.UserRepository
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseCookie
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.stereotype.Service
+import java.time.Duration
+import kotlin.jvm.optionals.getOrNull
+
+@Service
+class AuthService(
+    private val userRepository: UserRepository,
+    private val passwordEncoder: PasswordEncoder,
+    private val tokenService: TokenService,
+    private val bCryptPasswordEncoder: BCryptPasswordEncoder,
+    private val signUpMapper: SignUpMapper,
+    private val signInMapper: SignInMapper
+) {
+    fun signUp(request: SignUpRequest): SignUpResponse {
+        val entity = signUpMapper.toEntity(request)
+        entity.password = Password(bCryptPasswordEncoder.encode(request.password.value)!!)
+        val user = userRepository.save(entity)
+        return signUpMapper.toDto(user)
+    }
+
+    fun signIn(request: SignInRequest, response: HttpServletResponse): SignInResponse {
+        val user = userRepository.findByEmail(request.email).getOrNull()
+            ?: throw BadCredentialsException("User not found")
+
+        if (!passwordEncoder.matches(request.password.value, user.password.value)) {
+            throw BadCredentialsException("Invalid password")
+        }
+
+        val token = tokenService.generateToken(user.id!!)
+        val result = signInMapper.toDto(user, token)
+        addJwtCookieToResponse(token, response)
+
+        return result
+    }
+
+    private fun addJwtCookieToResponse(token: String, response: HttpServletResponse) {
+        val cookie = ResponseCookie.from("nativeflow-jwt-token", token)
+            .httpOnly(true)
+            .secure(true)
+            .path("/")
+            .sameSite("None")
+            .maxAge(Duration.ofDays(1))
+            .build()
+
+        response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString())
+    }
+}
