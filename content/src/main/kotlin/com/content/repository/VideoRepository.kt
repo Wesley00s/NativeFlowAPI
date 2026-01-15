f193a3466ab16d7de905a940dd1f468338c7e460
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:31:58 -0300

    feat(repository): Add methods to retrieve videos by status and title in VideoRepository

--- content/src/main/kotlin/com/content/repository/VideoRepository.kt
+++ content/src/main/kotlin/com/content/repository/VideoRepository.kt
@@ -1,9 +1,14 @@
 package com.content.repository
 
+import com.content.domain.enums.VideoStatus
 import com.content.domain.model.Video
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import java.util.Optional
 
 interface VideoRepository: MongoRepository<Video, String> {
     fun findBySourceId(sourceId: String): Optional<Video>
+    fun findAllByStatus(status: VideoStatus, pageable: Pageable): Page<Video>
+    fun findAllByStatusAndTitleContainingIgnoreCase(status: VideoStatus, title: String, pageable: Pageable): Page<Video>
 }
