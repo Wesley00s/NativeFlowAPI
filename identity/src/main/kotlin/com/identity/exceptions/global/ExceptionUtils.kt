f961151565e358184a3f8ac9dc53010a12ac98a6
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:29:04 -0300

    feat(exceptions): Add ExceptionsUtils for structured error response creation

---
+++ identity/src/main/kotlin/com/identity/exceptions/global/ExceptionUtils.kt
@@ -0,0 +1,23 @@
+package com.identity.exceptions.global
+
+import com.identity.exceptions.model.ExceptionDetails
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Component
+import java.time.Instant
+
+@Component
+object ExceptionsUtils {
+    fun createErrorResponse(
+        ex: Exception?,
+        title: String?,
+        status: HttpStatus
+    ): ResponseEntity<ExceptionDetails> {
+        val errors: MutableMap<String?, String?> = HashMap()
+        errors["cause"] = ex?.message
+
+        return ResponseEntity.status(status).body(
+            ExceptionDetails(title, Instant.now(), status.value(), ex?.javaClass?.getName(), errors)
+        )
+    }
+}
