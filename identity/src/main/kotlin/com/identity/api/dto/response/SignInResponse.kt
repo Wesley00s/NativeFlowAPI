5e94395dc75d032f839820a5521134da26ba98e7
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:30:39 -0300

    feat(response): Add SignInResponse data class for user sign-in response

---
+++ identity/src/main/kotlin/com/identity/api/dto/response/SignInResponse.kt
@@ -0,0 +1,13 @@
+package com.identity.api.dto.response
+
+import com.identity.domain.enums.UserRole
+import java.util.UUID
+
+data class SignInResponse(
+    val id: UUID,
+    val firstName: String,
+    val lastName: String,
+    val email: String,
+    val role: UserRole,
+    var token: String
+)
