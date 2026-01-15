14a4fb3318278773b09a1e37c273a79038677756
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-15 14:28:44 -0300

    feat(exceptions): Add ExceptionDetails data class for structured error handling

---
+++ identity/src/main/kotlin/com/identity/exceptions/model/ExceptionDetails.kt
@@ -0,0 +1,11 @@
+package com.identity.exceptions.model
+
+import java.time.Instant
+
+data class ExceptionDetails(
+    val title: String? = null,
+    val timestamp: Instant? = null,
+    val status: Int? = null,
+    val exception: String? = null,
+    val details: MutableMap<String?, String?> = HashMap()
+)
