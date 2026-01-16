a11bc6d1656010f5df8882969c09ce3d1153451e
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-16 19:29:30 -0300

    feat(identity/service): Add signOut and getMe methods and refactor to use UserMapper

--- identity/src/main/kotlin/com/identity/service/AuthService.kt
+++ identity/src/main/kotlin/com/identity/service/AuthService.kt
@@ -1,18 +1,20 @@
 package com.identity.service
 
+import com.common.constansts.SecurityConstants
 import com.identity.api.dto.request.SignInRequest
 import com.identity.api.dto.request.SignUpRequest
 import com.identity.api.dto.response.SignInResponse
-import com.identity.api.dto.response.SignUpResponse
+import com.identity.api.dto.response.UserResponse
 import com.identity.config.security.TokenService
-import com.identity.domain.mapper.SignInMapper
-import com.identity.domain.mapper.SignUpMapper
+import com.identity.domain.mapper.UserMapper
 import com.identity.domain.model.Password
+import com.identity.domain.model.User
 import com.identity.domain.repository.UserRepository
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseCookie
 import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.Authentication
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
@@ -25,14 +27,13 @@ class AuthService(
     private val passwordEncoder: PasswordEncoder,
     private val tokenService: TokenService,
     private val bCryptPasswordEncoder: BCryptPasswordEncoder,
-    private val signUpMapper: SignUpMapper,
-    private val signInMapper: SignInMapper
+    private val userMapper: UserMapper
 ) {
-    fun signUp(request: SignUpRequest): SignUpResponse {
-        val entity = signUpMapper.toEntity(request)
+    fun signUp(request: SignUpRequest): UserResponse {
+        val entity = userMapper.toEntity(request)
         entity.password = Password(bCryptPasswordEncoder.encode(request.password.value)!!)
         val user = userRepository.save(entity)
-        return signUpMapper.toDto(user)
+        return userMapper.toResponse(user)
     }
 
     fun signIn(request: SignInRequest, response: HttpServletResponse): SignInResponse {
@@ -45,11 +46,26 @@ class AuthService(
 
         val token = tokenService.generateToken(user.id!!)
         addJwtCookieToResponse(token, response)
-        return signInMapper.toDto(user)
+        return userMapper.toSignInResponse(user)
     }
 
+    fun signOut(response: HttpServletResponse) {
+        response.addHeader(
+            HttpHeaders.SET_COOKIE, ResponseCookie.from(SecurityConstants.AUTH_COOKIE_NAME, "")
+                .maxAge(Duration.ZERO)
+                .path("/")
+                .httpOnly(true)
+                .secure(true)
+                .build()
+                .toString()
+        )
+    }
+
+    fun getMe(auth: Authentication): UserResponse =
+        userMapper.toResponse(auth.principal as User)
+
     private fun addJwtCookieToResponse(token: String, response: HttpServletResponse) {
-        val cookie = ResponseCookie.from("nativeflow-jwt-token", token)
+        val cookie = ResponseCookie.from(SecurityConstants.AUTH_COOKIE_NAME, token)
             .httpOnly(true)
             .secure(true)
             .path("/")
