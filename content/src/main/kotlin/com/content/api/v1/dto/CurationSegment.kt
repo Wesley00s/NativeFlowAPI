5594ef672f73c5d5f67f0e943b80eb46f5a4f96b
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-18 17:41:05 -0300

    refactor(content/api/v1/dto): Remove CurationSegment, PatchSegmentRequest and VideoTaskRequest

--- content/src/main/kotlin/com/content/api/v1/dto/CurationSegment.kt
+++
@@ -1,8 +0,0 @@
-package com.content.api.v1.dto
-
-data class CurationSegment(
-    val id: String,
-    val start: Double,
-    val end: Double,
-    val text: String
-)
--- content/src/main/kotlin/com/content/api/v1/dto/PatchSegmentRequest.kt
+++
@@ -1,9 +0,0 @@
-package com.content.api.v1.dto
-
-
-data class PatchSegmentRequest(
-    val segmentId: String,
-    val text: String? = null,
-    val start: Double? = null,
-    val end: Double? = null
-)
