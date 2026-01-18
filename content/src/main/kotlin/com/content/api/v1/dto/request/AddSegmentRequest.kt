c54d302b98c3e72e1bbbeca52ed4348f9cd3a22c
Wesley00s <wesley300rodrigues@gmail.com>
2026-01-18 17:42:03 -0300

    refactor(content/api/v1/dto): Remove WorkerPayloads, AddSegmentRequest, and UpdateTextRequest

--- content/src/main/kotlin/com/content/api/v1/dto/request/AddSegmentRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.content.api.v1.dto.request
-
-data class AddSegmentRequest(
-    val start: Double,
-    val end: Double,
-    val text: String
-)
--- content/src/main/kotlin/com/content/api/v1/dto/request/UpdateTextRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.content.api.v1.dto.request
-
-data class UpdateTextRequest(
-    val text: String
-)
